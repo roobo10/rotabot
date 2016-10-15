5450ea70b267a82e089189fed307e039e0212d97
roobo10 <reuben.borg@gmail.com>
2016-10-15 16:58:59 +0000

    Update

--- rotabot.py
+++ rotabot.py
@@ -11,7 +11,7 @@ class Bot(object):
     def __init__(self):
         logging.debug("Creating BOT!")
         token = None
-        self._status = []
+        self._status = {}
         self.keep_running = True
         try:
             token = os.environ['SLACK_TOKEN']
@@ -39,11 +39,44 @@ class Bot(object):
     def _process_message(self, message):
         if message['type'] == "message":
             if "create rota" in message['text'].lower():
-                self._status.setdefault(message['user'],{})
-                self._client.rtm_send_message(message['channel'],"Who is on the rota? Please type the names, separated by commas.")
-                self._status[message['user']].setdefault('status','names') 
-            
-            logging.debug(self._status)
+                self._status[message['user']] = {}
+                self._status[message['user']] = {'status':'awaiting rota type'} 
+                self._client.rtm_send_message(message['channel'],"What kind of rota would you like to make? Please reply with 'OOH' or 'General Trim'.")            
+            if message['user'] in self._status:
+                p = self._status[message['user']]
+                if p['status'] == 'awaiting rota type':
+                    if message['text'].lower() == "ooh" or message['text'].lower() == "general trim":
+                        self._client.rtm_send_message(message['channel'],"Thanks! What day will the rota start on? Please enter it as YYYY/MM/DD.")
+                        self._status[message['user']] = {'status':'awaiting start date'} 
+                    else:
+                        self._client.rtm_send_message(message['channel'],"Sorry, that's  not a valid date.  Please try again.")
+                elif p['status'] == 'awaiting start date':
+                    try:
+                        self.start_date = time.strptime(message['text'],"%Y/%m%d")
+                        self._client.rtm_send_message(message['channel'],"OK! Can you please enter the last date for the rota in the same format?")
+                        self._status[message['user']] = {'status':'awaiting end date'} 
+                    except:
+                        self._client.rtm_send_message(message['channel'],"Sorry, that's not a valid date.  Please try again.")
+                elif p['status'] == 'awaiting end date':
+                    try:
+                        self.end_date = time.strptime(message['text'],"%Y/%m%d")
+                        self._client.rtm_send_message(message['channel'],"That's great!")
+                        self._client.rtm_send_message(message['channel'],"Who's on this rota? Please type the names, separated by commas.")
+                        self._status[message['user']] = {'status':'awaiting names'} 
+                    except:
+                        self._client.rtm_send_message(message['channel'],"Sorry, that's not a valid date.  Please try again.")
+                elif p['status'] == 'awaiting names':
+                    self.rota_names = [n.strip().title() for n in message['names'].split(',')]
+                    self._client.rtm_send_message(message['channel'],"OK! That's %d people.  Is that right? Yes or No" % (len(self.rota_names))
+                    p['status'] == 'awaiting names confirmation':
+                elif p['status'] == 'awaiting names confirmation':
+                    if message['text'].lower() == "yes":
+                        self._client.rtm_send_message(message['channel'],"Great!")
+                    else:
+                        self._client.rtm_send_message(message['channel'],"Oops! Let's try those names again. Please type the names, separated by commas.")
+                        p['status'] == 'awaiting names':
+                else:
+                    self._client.rtm_send_message("Sorry, I don't understand what you're saying! Type 'create rota' to restart.")
 
 if __name__ == "__main__":
     log_level = os.getenv("LOG_LEVEL", "DEBUG")
